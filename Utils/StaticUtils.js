dd5dc77507bfbea5b4bf8b7b43b226d502aa8fb6
Guten Ye <ywzhaifei@gmail.com>
2025-12-08 19:38:37 +0800

    remove StaticUtils.encodedUtf8ToByteArray

--- Utils/StaticUtils.js
+++ Utils/StaticUtils.js
@@ -1,14 +1,4 @@
 export default class StaticUtils {
-  static encodedUtf8ToByteArray(encoded) {
-    const ar = [];
-
-    for (let i = 0; i < encoded.length; i++) {
-      ar.push(encoded.charCodeAt(i));
-    }
-
-    return ar;
-  }
-
   static quoteIfString(value, quotingSymbol = '"') {
     return value.constructor == String
       ? `${quotingSymbol}${value}${quotingSymbol}`
